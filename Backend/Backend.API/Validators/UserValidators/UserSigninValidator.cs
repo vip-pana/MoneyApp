3feacc8346fe399af4f869e6554d030fd8691c97
Pana <vip_P4na@protonmail.com>
2024-01-05 22:15:45 +0100

    Creation e renaming signin validators

--- Backend/Backend.API/Validators/UserValidators/UserSigninValidator.cs
+++ Backend/Backend.API/Validators/UserValidators/UserSigninValidator.cs
@@ -3,9 +3,9 @@ using FluentValidation;
 
 namespace Backend.API.Validators.UserValidators
 {
-    public class UserValidator : AbstractValidator<User>
+    public class UserSigninValidator : AbstractValidator<User>
     {
-        public UserValidator()
+        public UserSigninValidator()
         {
             RuleFor(c => c.Name).NotEmpty().WithMessage("User email is required")
                 .MinimumLength(3).MaximumLength(50).WithMessage("User name must be between 3 and 50 chars.");
@@ -13,6 +13,12 @@ namespace Backend.API.Validators.UserValidators
                 .MinimumLength(3).MaximumLength(50).WithMessage("User name must be between 3 and 50 chars.");
             RuleFor(c => c.Email).NotEmpty().WithMessage("User email is required")
                 .MinimumLength(3).EmailAddress().WithMessage("User email not valid, please insert a correct email.");
+            RuleFor(c => c.Password).NotEmpty().WithMessage("The password is required.")
+            .MinimumLength(8).WithMessage("The password must have at least 8 characters.")
+            .Matches("[A-Z]").WithMessage("The password must have at least one uppercase letter.")
+            .Matches("[a-z]").WithMessage("The password must have at least one lowercase letter.")
+            .Matches("[0-9]").WithMessage("The password must have at least one number.");
+
         }
     }
 }
