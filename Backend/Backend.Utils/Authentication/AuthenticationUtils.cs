ddd76dd5689504007bb05e61c4cb86435d00f2d5
Pana <vip_P4na@protonmail.com>
2024-01-05 22:13:57 +0100

    Creation of the Utils library

---
+++ Backend/Backend.Utils/Authentication/AuthenticationUtils.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Backend.Utils.Authentication
+{
+    public static class AuthenticationUtils
+    {
+        public static string GenerateAccessToken(string jwtKey)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+               "issuer",
+               "audience",
+               expires: DateTime.Now.AddDays(90),
+               signingCredentials: signingCredentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string inputPassword, string hashedPassword)
+        {
+           return BCrypt.Net.BCrypt.Verify(inputPassword, hashedPassword);
+        }
+    }
+}
