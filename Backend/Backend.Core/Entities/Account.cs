8d979a349496b45a60d8a2acf926ba96b7dc74e8
Pana <vip_P4na@protonmail.com>
2024-01-06 20:25:01 +0100

    Removed Liquidity

--- Backend/Backend.Core/Entities/Account.cs
+++ Backend/Backend.Core/Entities/Account.cs
@@ -6,7 +6,6 @@ namespace Backend.Core.Entities
     {
         public required string Name { get; set; }
         public required User UserOwner { get; set; }
-        public double Liquidity { get; set; }
         public CurrencyEnum Currency { get; set; }
         public List<Category> Categories { get; set; }
         public List<Transaction> Transactions { get; set; }
