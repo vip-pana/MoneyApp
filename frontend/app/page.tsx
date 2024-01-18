9f3d70c25b3074023dfb935308535e633c2d607e
Pana <vip_P4na@protonmail.com>
2024-01-18 18:44:47 +0100

    frontend: implemented some stuff

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,9 +2,16 @@
 
 import React, { useState } from "react";
 import Login from "./login/page";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 export default function Page() {
   const [isLoggedIn, SetIsLoggedIn] = useState<boolean>(false);
 
-  return <>{isLoggedIn ? <></> : <Login />}</>;
+  const queryClient = new QueryClient();
+  
+  return <>
+  <QueryClientProvider client={queryClient}>
+    {isLoggedIn ? <></> : <Login />}
+  </QueryClientProvider>
+  </>;
 }
