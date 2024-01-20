95c5ca77145dab61d96bdd8d84fa8c0231b1e427
Pana <vip_P4na@protonmail.com>
2024-01-20 13:42:10 +0100

    chore: some cleanup of files

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,16 @@
 "use client";
 
-import React, { useState } from "react";
 import Login from "./login/page";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function Page() {
-  const [isLoggedIn, SetIsLoggedIn] = useState<boolean>(false);
-
   const queryClient = new QueryClient();
 
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        {isLoggedIn ? <></> : <Login />}
+        <Login />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </>
