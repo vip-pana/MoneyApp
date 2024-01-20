be70d06ce52e7149c93f6a25c9805d7ef03fd75b
Pana <vip_P4na@protonmail.com>
2024-01-20 13:44:01 +0100

    Chore:  cancelled query provider cause not work and add a toastoptions on chackra provider

--- frontend/components/base/Providers.tsx
+++ frontend/components/base/Providers.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider>{children}</ChakraProvider>
-    </QueryClientProvider>
+    <ChakraProvider
+      toastOptions={{
+        defaultOptions: {
+          position: "top-right",
+          isClosable: true,
+        },
+      }}
+    >
+      {children}
+    </ChakraProvider>
   );
 }
