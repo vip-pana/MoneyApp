e98cdd60d86159e8a390b3aec7b5e248e4db8797
Pana <vip_P4na@protonmail.com>
2024-02-04 19:19:05 +0100

    deleted a useless HStack

--- frontend/app/components/dashboard/transactions/transactionsSearchbar.tsx
+++ frontend/app/components/dashboard/transactions/transactionsSearchbar.tsx
@@ -23,74 +23,72 @@ const TransactionsSearchbar = () => {
     <Card>
       <CardBody>
         <HStack>
-          <HStack>
-            <FormControl minW={"180px"}>
-              <Select placeholder="Select Date Range">
-                <option>Custom range</option>
-                <option>Today</option>
-                <option>This week</option>
-                <option>This month</option>
-                <option>This year</option>
-              </Select>
-            </FormControl>
-            <FormControl>
-              <Input type="date" placeholder="Start date" />
-            </FormControl>
-            <FormControl>
-              <Input type="date" placeholder="End date" />
-            </FormControl>
-            <FormControl>
-              <Select placeholder="Categories">
-                <option>United Arab Emirates</option>
-                <option>Nigeria</option>
-              </Select>
-              {/* <FormHelperText>Questa sarà una multiselect</FormHelperText> */}
-            </FormControl>
-            <FormControl>
-              <Select placeholder={"Currency"}>
-                {(Object.keys(Currency) as Array<keyof typeof Currency>).map((value) => (
-                  <option key={value} value={value}>
-                    {value.toUpperCase()}
-                  </option>
-                ))}
-              </Select>
-            </FormControl>
-            <FormControl>
-              <Box
-                cursor="pointer"
-                borderRadius={"lg"}
-                borderWidth={"1px"}
-                boxShadow={"md"}
-                borderColor={"teal.600"}
-                px={5}
-                py={1}
-              >
+          <FormControl minW={"180px"}>
+            <Select placeholder="Select Date Range">
+              <option>Custom range</option>
+              <option>Today</option>
+              <option>This week</option>
+              <option>This month</option>
+              <option>This year</option>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <Input type="date" placeholder="Start date" />
+          </FormControl>
+          <FormControl>
+            <Input type="date" placeholder="End date" />
+          </FormControl>
+          <FormControl>
+            <Select placeholder="Categories">
+              <option>United Arab Emirates</option>
+              <option>Nigeria</option>
+            </Select>
+            {/* <FormHelperText>Questa sarà una multiselect</FormHelperText> */}
+          </FormControl>
+          <FormControl>
+            <Select placeholder={"Currency"}>
+              {(Object.keys(Currency) as Array<keyof typeof Currency>).map((value) => (
+                <option key={value} value={value}>
+                  {value.toUpperCase()}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <Box
+              cursor="pointer"
+              borderRadius={"lg"}
+              borderWidth={"1px"}
+              boxShadow={"md"}
+              borderColor={"teal.600"}
+              px={5}
+              py={1}
+            >
+              <input {...getInputProps()} hidden />
+              <Text {...getLabelProps()} textAlign={"center"}>
+                Income
+              </Text>
+            </Box>
+          </FormControl>
+          <FormControl>
+            <Box
+              cursor="pointer"
+              borderRadius={"lg"}
+              borderWidth={"1px"}
+              boxShadow={"md"}
+              borderColor={"red.600"}
+              px={5}
+              py={1}
+              {...htmlProps}
+            >
+              <Box {...getCheckboxProps()}>
                 <input {...getInputProps()} hidden />
                 <Text {...getLabelProps()} textAlign={"center"}>
-                  Income
+                  {state.isChecked ? "clicked" : "Expense"}
                 </Text>
               </Box>
-            </FormControl>
-            <FormControl>
-              <Box
-                cursor="pointer"
-                borderRadius={"lg"}
-                borderWidth={"1px"}
-                boxShadow={"md"}
-                borderColor={"red.600"}
-                px={5}
-                py={1}
-                {...htmlProps}
-              >
-                <Box {...getCheckboxProps()}>
-                  <input {...getInputProps()} hidden />
-                  <Text {...getLabelProps()} textAlign={"center"}>
-                    {state.isChecked ? "clicked" : "Expense"}
-                  </Text>
-                </Box>
-              </Box>
-            </FormControl>
-          </HStack>
+            </Box>
+          </FormControl>
           <Spacer />
           <FormControl maxW={"10%"}>
             <Button rightIcon={<LuSearch />} w={"full"} colorScheme="teal">
