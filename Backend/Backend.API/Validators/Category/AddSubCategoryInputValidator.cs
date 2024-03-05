1fc591402a21e0c8e684e774603099af385567ac
Pana <vip_P4na@protonmail.com>
2024-03-05 16:48:46 +0100

    Merge: deleted chackra ui dependencies and installed shadcn ui dependencies

---
+++ Backend/Backend.API/Validators/Category/AddSubCategoryInputValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Backend.API.Validators.Category
+{
+    public class AddSubCategoryInputValidator : AbstractValidator<AddSubCategoryInputValidator>
+    {
+        public AddSubCategoryInputValidator()
+        {
+            
+        }
+    }
+}
---
+++ Backend/Backend.API/Validators/Transaction/AddOrUpdateTransactionInputValidator.cs
@@ -0,0 +1,16 @@
+﻿using Backend.API.Types.Input.Transaction;
+using FluentValidation;
+
+namespace Backend.API.Validators.Transaction
+{
+    public class AddOrUpdateTransactionInputValidator : AbstractValidator<AddOrUpdateTransactionInput>
+    {
+        public AddOrUpdateTransactionInputValidator()
+        {
+            RuleFor(x => x.Transaction.Category).NotEmpty();
+            RuleFor(x => x.Transaction.DateTime).NotEmpty();
+            RuleFor(x => x.Transaction.Description).NotEmpty();
+            RuleFor(x => x.Transaction.Amount).NotEmpty().GreaterThan(0);
+        }
+    }
+}
